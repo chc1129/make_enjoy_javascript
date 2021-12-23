ad6967cf95f109186f6cc569d54fef4ea5f242c0
chc1129 <chc1129@gmail.com>
2021-12-23 21:23:23 +0900

    javascript prg fix

---
+++ chap03/functionCall.js
@@ -0,0 +1,14 @@
+const f1 = function (x) {
+    console.log("f1");
+    return x * x;
+};
+
+const f2 = function (x) {
+    console.log("f2");
+    const a = f1(x);
+};
+
+console.log("start");
+const a = f2(10);
+console.log(a);
+console.log("goal");
